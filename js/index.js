ab2b8af1be613449c891698f45250c7cd2d3d661
alebmondragon <alebmondragon@gmail.com>
2026-07-16 11:55:09 -0500

    Jueves event added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-function openLink(url) {
-    window.open(url, '_blank').focus();
-  }
+document.querySelectorAll('nav a').forEach(link=>{
+
+link.addEventListener('click',function(e){
+
+e.preventDefault();
+
+const section=document.querySelector(this.getAttribute('href'));
+
+section.scrollIntoView({
+
+behavior:'smooth'
+
+});
+
+});
+
+});
